5a77137c12a9d0cb6d5894bf5d1a2e9309840032
frimpoca <frimpoca@mail.uc.edu>
2023-10-26 18:32:15 -0400

    getting Loopy

--- src/Loop2.java
+++ src/Loop2.java
@@ -1,6 +1,6 @@
-public class Loop1 { // start of class
+public class Loop2 { // start of class
     public static void main(String [] args){ // start of main()
-        for (int i=0; i<=30; i++){ // watch out for i<=30
+        for (int i=30; i>=0; i--){ // watch out for i>=0 and i=30
             System.out.println(i); // print i
         }
     } // end of main
